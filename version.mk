7fd38ee448145a5cab4e056a50f846b1b0903544
André Pinela <sheffzor@gmail.com>
2017-01-22 12:51:01 +0000

    tasks: Enhancement to create ProtonAOSP OTA package
     * Add bacon target
    
    Change-Id: Id243c3099b4914ed3b53efc42c0c7dac9d56dea8
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- version.mk
+++ version.mk
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CUSTOM_ROM_VERSION := 12.3.0
+CUSTOM_ROM_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
+CUSTOM_ROM_VERSION := 12.1.0
 
 ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.build.date.custom=$(CUSTOM_ROM_BUILD_DATE) \
     ro.build.version.custom=$(CUSTOM_ROM_VERSION)
